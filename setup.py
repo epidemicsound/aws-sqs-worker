bd41be89509d776870df8a44e2f3eb5917ff07b0
Fredrik Rosenqvist <fredrik.rosenqvist@epidemicsound.com>
2019-07-01 15:29:59 +0200

    Prepare for bumping version to v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='aws-sqs-worker',
-    version='0.0.1',
+    version='0.0.2',
     description='AWS SQS Worker Module',
     license='MIT',
     long_description=long_description,
